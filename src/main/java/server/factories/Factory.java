3072ff58fbe0dda60aac72611b8d7da517e00f82
mbragg02 <mikeabragg@icloud.com>
2014-04-20 09:48:22 +0100

    RMISecurityManager changed to SecurityManager

--- src/main/java/server/factories/Factory.java
+++ src/main/java/server/factories/Factory.java
@@ -4,7 +4,6 @@ import server.models.ServerData;
 import server.models.ServerImpl;
 import server.utilities.ShutdownHook;
 
-import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.util.Properties;
 import java.util.logging.ConsoleHandler;
@@ -41,8 +40,8 @@ public class Factory {
         return new ShutdownHook(serverData);
     }
 
-    public RMISecurityManager getRMISecurityManager() {
-        return new RMISecurityManager();
+    public SecurityManager getRMISecurityManager() {
+        return new SecurityManager();
     }
 
     public ServerImpl getServer(QuizFactory quizFactory, ServerData serverData) throws RemoteException {
